f71f2ffbadce4ab4fd791eaa603e7d5689d37df8
val_pinkman <design.valentin@gmail.com>
2014-08-22 12:15:43 +0200

    instanciation de mDecodeManager dans la classe Honeywellscanner

--- src/android/com/honeywell/scanner/HoneywellScanner.java
+++ src/android/com/honeywell/scanner/HoneywellScanner.java
@@ -12,8 +12,25 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 
+import com.honeywell.decodemanager.DecodeManager;
+import com.honeywell.decodemanager.barcode.CommonDefine;
+import com.honeywell.decodemanager.barcode.DecodeResult;
+
 public class HoneywellScanner extends CordovaPlugin {
     public static final int REQUEST_CODE = 0x0ba7c0de;
+
+    private final int ID_CLEAR_SCREEN = 0x13;
+
+    public static final int SymbologySettingMenu=0x7f050001;
+    public static final int ClearScreenMenu=0x7f050002;
+    public static final int PROMOT_CLICK_SCAN_BUTTON=0x7f050004;
+
+    private DecodeManager mDecodeManager = null;
+    private EditText mDecodeResultEdit = null;
+    private final int SCANKEY = 0x94;
+    private final int SCANTIMEOUT = 5000;
+    long mScanAccount = 0;
+    private boolean mbKeyDown = true;
 
     private static final String TRIGGER = "trigger";
     private static final String TRIGGER_INTENT = "com.honeywell.scanner.TRIGGER";
@@ -31,8 +48,17 @@ public class HoneywellScanner extends CordovaPlugin {
     }
 
     @Override
+    public void onResume(boolean multitasking) {
+        super.onResume();
+
+        if (mDecodeManager == null) {
+            mDecodeManager = new DecodeManager(this.cordova.getActivity().getApplicationContext(), ScanResultHandler);
+        }
+        // this.mDecodeManager = new DecodeManager(this.cordova.getActivity().getApplicationContext(), ScanResultHandler);
+    }
+
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-        this.callbackContext = callbackContext;
 
         if(action.equals(TRIGGER)) {
             trigger();
@@ -42,60 +68,51 @@ public class HoneywellScanner extends CordovaPlugin {
         return true;
     }
 
-    // @Override
-    // public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    //     this.callbackContext = callbackContext;
-    //     if (action.equals(TRIGGER)) {
-    //         final long duration = args.getLong(0);
-    //         cordova.getActivity().runOnUiThread(new Runnable() {
-    //             public void run() {
-    //                 trigger();
-    //                 callbackContext.success(); // Thread-safe.
-    //             }
-    //         });
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
     public void trigger() {
-        Intent intentTrigger = new Intent(TRIGGER_INTENT);
-        intentTrigger.addCategory(Intent.CATEGORY_DEFAULT);
-
-        intentTrigger.setPackage(this.cordova.getActivity().getApplicationContext().getPackageName());
-        this.cordova.startActivityForResult((CordovaPlugin) this, intentTrigger, REQUEST_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put(TEXT, intent.getStringExtra("SCAN_RESULT"));
-                    obj.put(FORMAT, intent.getStringExtra("SCAN_RESULT_FORMAT"));
-                    obj.put(CANCELLED, false);
-                } catch (JSONException e) {
-                    Log.d(LOG_TAG, "This should never happen");
-                }
-                this.callbackContext.success(obj);
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                JSONObject obj = new JSONObject();
-                try {
-                    obj.put(TEXT, "");
-                    obj.put(FORMAT, "");
-                    obj.put(CANCELLED, true);
-                } catch (JSONException e) {
-                    Log.d(LOG_TAG, "This should never happen");
-                }
-                //this.success(new PluginResult(PluginResult.Status.OK, obj), this.callback);
-                this.callbackContext.success(obj);
-            } else {
-                this.callbackContext.error("Unexpected error");
-            }
+        try {
+            mDecodeManager.doDecode(SCANTIMEOUT);
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
+    private Handler ScanResultHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case DecodeManager.MESSAGE_DECODER_COMPLETE:
+                mScanAccount++;
+                String strDecodeResult = "";
+                DecodeResult decodeResult = (DecodeResult) msg.obj;
+                // SoundManager.playSound(1, 1);
 
+                byte codeid = decodeResult.codeId;
+                byte aimid = decodeResult.aimId;
+                int iLength = decodeResult.length;
+
+                strDecodeResult = "Decode Result::"+ decodeResult.barcodeData + "\r\n" + "codeid::"+ "(" + String.valueOf((char) codeid) +"/"+  String.valueOf((char) aimid)+")" + "\r\n" + "Length:: " + iLength
+                        + "  " + "Count:: " + mScanAccount + "\r\n";
+
+                mDecodeResultEdit.setText(strDecodeResult);
+                break;
+
+            case DecodeManager.MESSAGE_DECODER_FAIL: {
+                // SoundManager.playSound(2, 1);
+                mDecodeResultEdit.setText("Decode Result::Scan fail");
+
+            }
+            break;
+            case DecodeManager.MESSAGE_DECODER_READY:
+            {
+                ArrayList<java.lang.Integer> arry =  mDecodeManager.getSymConfigActivityOpeartor().getAllSymbologyId();
+                boolean b = arry.isEmpty();
+            }
+            break;
+            default:
+                super.handleMessage(msg);
+                break;
+            }
+        }
+    };
 
 }
--- src/android/com/honeywell/scanner/ScanDemoMainActivity.java
+++ src/android/com/honeywell/scanner/ScanDemoMainActivity.java
@@ -1,180 +1,180 @@
-package com.honeywell.scanner;
+// package com.honeywell.scanner;
 
 
-import android.app.Activity;
+// import android.app.Activity;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.RemoteException;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import java.io.IOException;
-import java.util.ArrayList;
+// import android.os.Bundle;
+// import android.os.Handler;
+// import android.os.Message;
+// import android.os.RemoteException;
+// import android.view.KeyEvent;
+// import android.view.Menu;
+// import android.view.MenuItem;
+// import android.view.MotionEvent;
+// import android.view.View;
+// import android.view.Window;
+// import android.view.WindowManager;
+// import android.widget.Button;
+// import android.widget.EditText;
+// import java.io.IOException;
+// import java.util.ArrayList;
 
-import com.honeywell.decodemanager.DecodeManager;
-import com.honeywell.decodemanager.barcode.DecodeResult;
+// import com.honeywell.decodemanager.DecodeManager;
+// import com.honeywell.decodemanager.barcode.DecodeResult;
 
 
-public final class ScanDemoMainActivity extends Activity {
+// public final class ScanDemoMainActivity extends Activity {
 
-	private final int ID_SCANSETTING = 0x12;
-	private final int ID_CLEAR_SCREEN = 0x13;
+// 	private final int ID_SCANSETTING = 0x12;
+// 	private final int ID_CLEAR_SCREEN = 0x13;
 
-	public static final int SymbologySettingMenu=0x7f050001;
-	public static final int ClearScreenMenu=0x7f050002;
-	public static final int PROMOT_CLICK_SCAN_BUTTON=0x7f050004;
+// 	public static final int SymbologySettingMenu=0x7f050001;
+// 	public static final int ClearScreenMenu=0x7f050002;
+// 	public static final int PROMOT_CLICK_SCAN_BUTTON=0x7f050004;
 
-	private DecodeManager mDecodeManager = null;
-	private EditText mDecodeResultEdit = null;
-    private final int SCANKEY = 0x94;
-	private final int SCANTIMEOUT = 5000;
-	long mScanAccount = 0;
-	private boolean mbKeyDown = true;
+// 	private DecodeManager mDecodeManager = null;
+// 	private EditText mDecodeResultEdit = null;
+//     private final int SCANKEY = 0x94;
+// 	private final int SCANTIMEOUT = 5000;
+// 	long mScanAccount = 0;
+// 	private boolean mbKeyDown = true;
 
-	// private String strResultM;
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+// 	// private String strResultM;
+// 	/** Called when the activity is first created. */
+// 	@Override
+// 	public void onCreate(Bundle savedInstanceState) {
+// 		super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+// 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+// 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+// 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		// setContentView(R.layout.main);
-		// initializeUI();
+// 		// setContentView(R.layout.main);
+// 		// initializeUI();
 
-	}
+// 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+// 	@Override
+// 	protected void onResume() {
+// 		super.onResume();
 
-		if (mDecodeManager == null) {
-			mDecodeManager = new DecodeManager(this,ScanResultHandler);
-		}
+// 		if (mDecodeManager == null) {
+// 			mDecodeManager = new DecodeManager(this,ScanResultHandler);
+// 		}
 
-		// SoundManager.getInstance();
-		// SoundManager.initSounds(getBaseContext());
-		// SoundManager.loadSounds();
-	}
+// 		// SoundManager.getInstance();
+// 		// SoundManager.initSounds(getBaseContext());
+// 		// SoundManager.loadSounds();
+// 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		menu.add(0, ID_SCANSETTING, 0, SymbologySettingMenu);
-		menu.add(0, ID_CLEAR_SCREEN, 1, ClearScreenMenu);
+// 	@Override
+// 	public boolean onCreateOptionsMenu(Menu menu) {
+// 		super.onCreateOptionsMenu(menu);
+// 		menu.add(0, ID_SCANSETTING, 0, SymbologySettingMenu);
+// 		menu.add(0, ID_CLEAR_SCREEN, 1, ClearScreenMenu);
 
-		return true;
-	}
+// 		return true;
+// 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+// 	@Override
+// 	public boolean onOptionsItemSelected(MenuItem item) {
+// 		switch (item.getItemId()) {
 
-		case ID_SCANSETTING: {
-			mDecodeManager.getSymConfigActivityOpeartor().start();
-			 return true;
-		}
-		case ID_CLEAR_SCREEN: {
+// 		case ID_SCANSETTING: {
+// 			mDecodeManager.getSymConfigActivityOpeartor().start();
+// 			 return true;
+// 		}
+// 		case ID_CLEAR_SCREEN: {
 
-			String strPromotScan = this.getResources().getString(PROMOT_CLICK_SCAN_BUTTON);
-			mDecodeResultEdit.setText(strPromotScan);
+// 			String strPromotScan = this.getResources().getString(PROMOT_CLICK_SCAN_BUTTON);
+// 			mDecodeResultEdit.setText(strPromotScan);
 
-			return true;
-		}
-		default:
-			return false;
-		}
-	}
+// 			return true;
+// 		}
+// 		default:
+// 			return false;
+// 		}
+// 	}
 
-	@Override
-	protected void onPause() {
-		super.onPause();
+// 	@Override
+// 	protected void onPause() {
+// 		super.onPause();
 
-		if (mDecodeManager != null) {
-			try {
-				mDecodeManager.release();
-				mDecodeManager = null;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+// 		if (mDecodeManager != null) {
+// 			try {
+// 				mDecodeManager.release();
+// 				mDecodeManager = null;
+// 			} catch (IOException e) {
+// 				// TODO Auto-generated catch block
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
+// 	@Override
+// 	public void onDestroy() {
+// 		super.onDestroy();
 
-		// SoundManager.cleanup();
-		if (mDecodeManager != null) {
-			try {
+// 		// SoundManager.cleanup();
+// 		if (mDecodeManager != null) {
+// 			try {
 
-				mDecodeManager.release();
-				mDecodeManager = null;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+// 				mDecodeManager.release();
+// 				mDecodeManager = null;
+// 			} catch (IOException e) {
+// 				// TODO Auto-generated catch block
+// 				e.printStackTrace();
+// 			}
+// 		}
+// 	}
 
-	private void DoScan() throws Exception {
-		try {
-			mDecodeManager.doDecode(SCANTIMEOUT);
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+// 	private void DoScan() throws Exception {
+// 		try {
+// 			mDecodeManager.doDecode(SCANTIMEOUT);
+// 		} catch (RemoteException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		}
+// 	}
 
-	private Handler ScanResultHandler = new Handler() {
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case DecodeManager.MESSAGE_DECODER_COMPLETE:
-				mScanAccount++;
-				String strDecodeResult = "";
-				DecodeResult decodeResult = (DecodeResult) msg.obj;
-				// SoundManager.playSound(1, 1);
+// 	private Handler ScanResultHandler = new Handler() {
+// 		public void handleMessage(Message msg) {
+// 			switch (msg.what) {
+// 			case DecodeManager.MESSAGE_DECODER_COMPLETE:
+// 				mScanAccount++;
+// 				String strDecodeResult = "";
+// 				DecodeResult decodeResult = (DecodeResult) msg.obj;
+// 				// SoundManager.playSound(1, 1);
 
-				byte codeid = decodeResult.codeId;
-				byte aimid = decodeResult.aimId;
-				int iLength = decodeResult.length;
+// 				byte codeid = decodeResult.codeId;
+// 				byte aimid = decodeResult.aimId;
+// 				int iLength = decodeResult.length;
 
-				strDecodeResult = "Decode Result::"+ decodeResult.barcodeData + "\r\n" + "codeid::"+ "(" + String.valueOf((char) codeid) +"/"+  String.valueOf((char) aimid)+")" + "\r\n" + "Length:: " + iLength
-						+ "  " + "Count:: " + mScanAccount + "\r\n";
+// 				strDecodeResult = "Decode Result::"+ decodeResult.barcodeData + "\r\n" + "codeid::"+ "(" + String.valueOf((char) codeid) +"/"+  String.valueOf((char) aimid)+")" + "\r\n" + "Length:: " + iLength
+// 						+ "  " + "Count:: " + mScanAccount + "\r\n";
 
-				mDecodeResultEdit.setText(strDecodeResult);
-				break;
+// 				mDecodeResultEdit.setText(strDecodeResult);
+// 				break;
 
-			case DecodeManager.MESSAGE_DECODER_FAIL: {
-				// SoundManager.playSound(2, 1);
-				mDecodeResultEdit.setText("Decode Result::Scan fail");
+// 			case DecodeManager.MESSAGE_DECODER_FAIL: {
+// 				// SoundManager.playSound(2, 1);
+// 				mDecodeResultEdit.setText("Decode Result::Scan fail");
 
-			}
-			break;
-			case DecodeManager.MESSAGE_DECODER_READY:
-			{
-				ArrayList<java.lang.Integer> arry =  mDecodeManager.getSymConfigActivityOpeartor().getAllSymbologyId();
-				boolean b = arry.isEmpty();
-			}
-			break;
-			default:
-				super.handleMessage(msg);
-				break;
-			}
-		}
-	};
-	private void cancleScan() throws Exception {
-		mDecodeManager.cancelDecode();
-	}
-}
+// 			}
+// 			break;
+// 			case DecodeManager.MESSAGE_DECODER_READY:
+// 			{
+// 				ArrayList<java.lang.Integer> arry =  mDecodeManager.getSymConfigActivityOpeartor().getAllSymbologyId();
+// 				boolean b = arry.isEmpty();
+// 			}
+// 			break;
+// 			default:
+// 				super.handleMessage(msg);
+// 				break;
+// 			}
+// 		}
+// 	};
+// 	private void cancleScan() throws Exception {
+// 		mDecodeManager.cancelDecode();
+// 	}
+// }

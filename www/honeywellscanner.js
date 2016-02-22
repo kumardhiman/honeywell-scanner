1143586e93780ae8c67faa90ca5be38b75880725
kumardhiman <kumar@ksunnyd.com>
2016-02-22 11:34:29 +0000

    Update honeywellscanner.js

--- www/honeywellscanner.js
+++ www/honeywellscanner.js
@@ -1,14 +1,32 @@
-var exec = require("cordova/exec");
+//var exec = require("cordova/exec");
+//function honeywellScanner () {
+//};
+//honeywellScanner.prototype.initialize = function(onSuccess, onError) {
+//    exec(onSuccess, onError, 'HoneywellScanner', 'initialize', []);
+//};
+//honeywellScanner.prototype.trigger = function(onSuccess, onError) {
+//    exec(onSuccess, onError, 'HoneywellScanner', 'trigger', []);
+//};
+//var scanner = new honeywellScanner();
+//module.exports = scanner;
+var cordova = require('cordova');
+var exec = require('cordova/exec');
 
-function honeywellScanner () {
+var HoneywellScannerPlugin = function() {
+    this.initialize = function(success_cb, error_cb){
+    exec(success_cb, error_cb, "HoneywellScanner", "initialize", []);
+  };
+  //this.scan = function(success_cb, error_cb){
+    //exec(success_cb, error_cb, "HoneywellScanner", "initialize", []);
+  //};
+this.scan  = function(success_cb, error_cb){
+    exec(success_cb, error_cb, "HoneywellScanner", "trigger", []);
+  };
+  //this.startListen = function(success_cb, error_cb){
+//    exec(success_cb, error_cb, "BarcodeScannerPlugin", "start", []);
+//  };
+
 };
 
-honeywellScanner.prototype.initialize = function(onSuccess, onError) {
-    exec(onSuccess, onError, 'HoneywellScanner', 'initialize', []);
-};
-
-honeywellScanner.prototype.trigger = function(onSuccess, onError) {
-    exec(onSuccess, onError, 'HoneywellScanner', 'trigger', []);
-};
-var scanner = new honeywellScanner();
-module.exports = scanner;
+var honeywellScannerPlugin = new HoneywellScannerPlugin();
+module.exports = honeywellScannerPlugin;

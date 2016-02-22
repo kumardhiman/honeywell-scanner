ef5f5c635bda6202b96bf335ea19d1b0a5f6117d
kumardhiman <kumar@ksunnyd.com>
2016-02-22 11:38:08 +0000

    Update honeywellscanner.js

--- www/honeywellscanner.js
+++ www/honeywellscanner.js
@@ -1,14 +1,14 @@
-var exec = require("cordova/exec");
+var cordova = require('cordova');
+var exec = require('cordova/exec');
 
-function honeywellScanner () {
+var HoneywellScannerPlugin = function() {
+    this.initialize = function(success_cb, error_cb){
+    exec(success_cb, error_cb, "HoneywellScanner", "initialize", []);
+  };
+this.scan  = function(success_cb, error_cb){
+    exec(success_cb, error_cb, "HoneywellScanner", "trigger", []);
+  };
 };
 
-honeywellScanner.prototype.initialize = function(onSuccess, onError) {
-    exec(onSuccess, onError, 'HoneywellScanner', 'initialize', []);
-};
-
-honeywellScanner.prototype.trigger = function(onSuccess, onError) {
-    exec(onSuccess, onError, 'HoneywellScanner', 'trigger', []);
-};
-var scanner = new honeywellScanner();
-module.exports = scanner;
+var honeywellScannerPlugin = new HoneywellScannerPlugin();
+module.exports = honeywellScannerPlugin;
